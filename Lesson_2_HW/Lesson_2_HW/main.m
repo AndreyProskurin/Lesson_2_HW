5cfd9c6624dfddec4cb9d03296e97af69cb00adf
Andrey Proskurin <andrewproskurin@gmail.com>
2017-08-12 21:28:04 +0300

    Переменные.

--- Lesson_2_HW/Lesson_2_HW/main.m
+++ Lesson_2_HW/Lesson_2_HW/main.m
@@ -10,8 +10,65 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        // Variables
+        
+        // BOOL
+        
+        BOOL isItTrue = YES;
+        isItTrue = NO;
+        
+        NSLog(@"Now BOOL value = %hhd.", isItTrue);
+        
+        // NSInteger
+        
+        NSInteger integerValue_1 = 10;
+        NSInteger integerValue_2 = -5;
+        NSInteger integerResult = 0;
+        
+        integerResult = integerValue_1 + integerValue_2;
+        NSLog(@"Result of addition = %ld.", integerResult);
+        
+        integerResult = integerValue_2 - integerValue_1;
+        NSLog(@"Result of subtraction = %ld.", integerResult);
+        
+        // CGFloat
+        
+        CGFloat floatValue_1 = 3.3f;
+        CGFloat floatValue_2 = -2.4f;
+        CGFloat floatResult = 0.f;
+        
+        floatResult = -(floatValue_1) * floatValue_2;
+        NSLog(@"Result of multiplication = %.2f.", floatResult);
+        
+        floatResult = floatValue_1 / floatValue_2;
+        NSLog(@"Result of division = %.2f.", floatResult);
+        
+        // NSNumber
+        
+        NSNumber *unknownVariable = nil;
+        
+        unknownVariable = @(isItTrue);
+        NSLog(@"Now unknownVariable = %@.", unknownVariable);
+        
+        unknownVariable = @(integerResult);
+        NSLog(@"And now unknownVariable = %@.", unknownVariable);
+        
+        unknownVariable = [[NSNumber alloc] initWithFloat:floatResult];
+        NSLog(@"And finally unknownVariable = %@.", unknownVariable);
+        
+        // NSString
+        
+        NSString *warning;
+        
+        warning = @"Don't press this button!!!";
+        warning = @"If you press this one, the computer will switch off!";
+        
+        // NSArray
+        
+        NSArray *listOfVariables = @[@(isItTrue), @(integerResult), @(floatResult), unknownVariable, warning];
+        NSLog(@"%ld.", [listOfVariables indexOfObject:unknownVariable]);
+        
     }
     return 0;
 }
