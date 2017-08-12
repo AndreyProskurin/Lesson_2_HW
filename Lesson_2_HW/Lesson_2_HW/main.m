7e4f75b46da7451db526076e697b0cae3ed658d1
Andrey Proskurin <andrewproskurin@gmail.com>
2017-08-13 01:39:22 +0300

    Циклы.

--- Lesson_2_HW/Lesson_2_HW/main.m
+++ Lesson_2_HW/Lesson_2_HW/main.m
@@ -10,8 +10,40 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        // Cycles
+        
+        // For-cycle
+        
+        for (NSInteger i = 0; i <=20; i++) {
+            if (i % 5 == 0 && i != 0) {
+                NSLog(@"Число %ld кратно 5.", i);
+            }
+        }
+        
+        // Do-While cycle
+        
+        NSInteger count = 0;
+        
+        do {
+            count += 7;
+            count++;
+        } while (count < 50);
+        
+        NSLog(@"%ld", count);
+        NSLog(@"Numbers of this cycle's iterations is %ld.", count / 7);
+        
+        // While-cycle
+        
+        count = 0;
+        
+        while (count < 100) {
+            count += 3;
+            if (count > 30) {
+                NSLog(@"\"count\" = %ld. It's more than 30, so we have broken this cycle.", count);
+                break;
+            }
+        }
     }
     return 0;
 }
