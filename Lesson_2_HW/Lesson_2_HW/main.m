a0a8282d72c819c3a0da682098714c350b34c868
Andrey Proskurin <andrewproskurin@gmail.com>
2017-08-15 12:40:55 +0300

    Дополнительное ДЗ.

--- Lesson_2_HW/Lesson_2_HW/main.m
+++ Lesson_2_HW/Lesson_2_HW/main.m
@@ -7,11 +7,56 @@
 //
 
 #import <Foundation/Foundation.h>
+#define SWAP(A, B) { int t = A; A = B; B = t; }
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        NSMutableArray *arrayOfRandomNumbers = [[NSMutableArray alloc] init];
+        
+        for (NSInteger i = 0; i < 100; i++) {
+            NSInteger randomValue = arc4random() % 120;
+            [arrayOfRandomNumbers addObject:@(randomValue)];
+        }
+        
+        NSNumber *min = arrayOfRandomNumbers[0];
+        NSNumber *max;
+        
+        for (NSInteger i = 0; i < 100; i++) {
+            if (arrayOfRandomNumbers[i] < min) {
+                min = arrayOfRandomNumbers[i];
+            } else if (arrayOfRandomNumbers[i] >= max) {
+                max = arrayOfRandomNumbers[i];
+            }
+        }
+        
+        [arrayOfRandomNumbers sortUsingSelector:@selector(compare:)];  // Не получилось с сортировкой :(
+        
+        // Не получилось.
+        
+//        for (NSInteger i = 0; i < arrayOfRandomNumbers.count - 1; i++) {
+//            for (NSInteger j = 0; j < i; j++) {
+//                if ([arrayOfRandomNumbers[i] integerValue] > [arrayOfRandomNumbers[i+1] integerValue]) {
+//                    NSNumber *temp = arrayOfRandomNumbers[j];
+//                    arrayOfRandomNumbers[j] = arrayOfRandomNumbers[j+1];
+//                    arrayOfRandomNumbers[j+1] = temp;
+//
+//                }
+//            }
+//
+//        }
+        
+        NSLog(@"Capacity of the array = %ld", arrayOfRandomNumbers.count);
+        NSLog(@"Minimum = %@; Maximum = %@.", min, max);
+        
+        NSMutableString *resultMutableString = [NSMutableString new];
+        for (NSInteger i = 0; i < arrayOfRandomNumbers.count; i++) {
+
+            [resultMutableString appendString:[arrayOfRandomNumbers[i] stringValue]];
+            [resultMutableString appendString:@" | "];
+        }
+        
+        NSLog(@"\n\nRESULT: %@", resultMutableString);
     }
     return 0;
 }
